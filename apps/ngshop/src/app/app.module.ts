d06bab239a5d70a9d7fc4d88c9b57199990649b1
OnoprienkoArtem <o.artem.e@gmail.com>
2022-07-07 23:55:25 +0300

    set basic structure of styles

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -23,7 +22,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
+    declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
     imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
     providers: [],
     bootstrap: [AppComponent]
