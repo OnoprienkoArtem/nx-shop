dd0fbc73b6f4b6591810f16710cc9635f2d96be5
OnoprienkoArtem <o.artem.e@gmail.com>
2022-08-01 23:57:41 +0300

    add service method for creating category and adding to db

--- apps/admin/src/app/categories/categories-form/categories-form.component.ts
+++ apps/admin/src/app/categories/categories-form/categories-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CategoriesService, Category } from '@bluebits/products';
 
 @Component({
   selector: 'admin-categories-form',
@@ -11,7 +12,10 @@ export class CategoriesFormComponent implements OnInit {
   form: FormGroup;
   isSubmitted = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private categoriesService: CategoriesService
+  ) { }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
@@ -25,8 +29,16 @@ export class CategoriesFormComponent implements OnInit {
     if (this.form.invalid) {
       return;
     }
+
+    const category: Category = {
+      name: this.categoryForm['name'].value,
+      icon: this.categoryForm['icon'].value,
+    };
+
     console.log(this.categoryForm['name'].value);
     console.log(this.categoryForm['icon'].value);
+
+    this.categoriesService.createCategory(category).subscribe();
   }
 
   get categoryForm() {
