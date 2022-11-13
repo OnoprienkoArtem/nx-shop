92e901bae21d6c276b3162f62625550d63f3c51e
artemonoprienko <o.artem.e@gmail.com>
2022-11-14 01:05:53 +0200

    define error messages

--- libs/users/src/lib/pages/login/login.component.ts
+++ libs/users/src/lib/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { AuthService } from "../../services/auth.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'users-login',
@@ -11,6 +12,8 @@ import { AuthService } from "../../services/auth.service";
 export class LoginComponent implements OnInit {
   loginFormGroup!: FormGroup;
   isSubmitted = false;
+  authError = false;
+  authMessage = 'Email or password are wrong!';
 
   constructor(private formBuilder: FormBuilder, private auth: AuthService) { }
 
@@ -34,8 +37,17 @@ export class LoginComponent implements OnInit {
 
     if (this.loginFormGroup.invalid) return;
 
-    this.auth.login(this.loginForm['email'].value, this.loginForm['password'].value).subscribe(user => {
-      console.log(user);
-    });
+    this.auth.login(this.loginForm['email'].value, this.loginForm['password'].value).subscribe(
+      user => {
+        console.log(user);
+        this.authError = false;
+      },
+      (error: HttpErrorResponse) => {
+        this.authError = true;
+        if (error.status !== 400) {
+          this.authMessage = 'Error in the server, please try again later!';
+        }
+      },
+    );
   }
 }
