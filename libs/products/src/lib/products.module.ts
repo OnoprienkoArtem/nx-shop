6239bb93cd4934d97f5c5835cc323f4a2767b98d
artemonoprienko <o.artem.e@gmail.com>
2023-01-13 23:55:45 +0200

    added categories-banner component

--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { ProductsSearchComponent } from './components/products-search/products-search.component';
+import { CategoriesBannerComponent } from './components/categories-banner/categories-banner.component';
 
 export const productsRoutes: Route[] = [];
 
 @NgModule({
     imports: [CommonModule, RouterModule],
-    declarations: [ProductsSearchComponent],
-    exports: [ProductsSearchComponent]
+    declarations: [ProductsSearchComponent, CategoriesBannerComponent],
+    exports: [ProductsSearchComponent, CategoriesBannerComponent]
 })
 export class ProductsModule {}
