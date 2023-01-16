2812338e06fddd3e60dfcbefcda68374b6a075dc
artemonoprienko <o.artem.e@gmail.com>
2023-01-16 23:56:18 +0200

    replace products list to lib

--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -1,27 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Route } from '@angular/router';
+import { RouterModule, Route, Routes } from '@angular/router';
 import { ProductsSearchComponent } from './components/products-search/products-search.component';
 import { CategoriesBannerComponent } from './components/categories-banner/categories-banner.component';
 import { FeaturedProductsComponent } from './components/featured-products/featured-products.component';
 import { ButtonModule } from 'primeng/button';
 import { ProtuctItemComponent } from './components/product-item/product-item.component';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
 
-export const productsRoutes: Route[] = [];
+export const productsRoutes: Routes = [
+  {
+    path: 'products',
+    component: ProductsListComponent
+  }
+];
 
 @NgModule({
-    imports: [CommonModule, RouterModule, ButtonModule],
+    imports: [CommonModule, RouterModule.forChild(productsRoutes), ButtonModule],
     declarations: [
       ProductsSearchComponent,
       CategoriesBannerComponent,
       ProtuctItemComponent,
       FeaturedProductsComponent,
+      ProductsListComponent,
     ],
     exports: [
       ProductsSearchComponent,
       CategoriesBannerComponent,
       ProtuctItemComponent,
       FeaturedProductsComponent,
+      ProductsListComponent,
     ]
 })
 export class ProductsModule {}
