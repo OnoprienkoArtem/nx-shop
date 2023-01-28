aafd7fb5889220c9a5e4870a81539fa679380135
artemonoprienko <o.artem.e@gmail.com>
2023-01-28 18:09:59 +0200

    fetch product by id from api

--- libs/products/src/lib/pages/product-details/product-details.component.ts
+++ libs/products/src/lib/pages/product-details/product-details.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject, takeUntil, tap } from 'rxjs';
+import { Product } from '../../models/product';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'products-product-details',
   templateUrl: './product-details.component.html',
-  styles: [
-  ]
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductDetailsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  product!: Product;
+  unSub$: Subject<any> = new Subject();
+
+  constructor(
+    private productsService: ProductsService,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      if (params['productId']) {
+        this.getProduct(params['productId']);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unSub$.next(true);
+    this.unSub$.complete();
+  }
+
+  private getProduct(id: string) {
+    this.productsService.getProduct(id)
+      .pipe(
+        tap(resProduct => this.product = resProduct),
+        takeUntil(this.unSub$),
+      )
+      .subscribe();
   }
 
 }
