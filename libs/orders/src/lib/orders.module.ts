48b0ae6e683ea71618f18e32236d854406faedbb
artemonoprienko <o.artem.e@gmail.com>
2023-06-03 21:51:06 +0300

    create cart-icon component

--- libs/orders/src/lib/orders.module.ts
+++ libs/orders/src/lib/orders.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { CartService } from './services/cart.service';
+import { CartIconComponent } from './components/cart-icon/cart-icon.component';
 
 export const ordersRoutes: Route[] = [];
 
 @NgModule({
-    imports: [CommonModule, RouterModule]
+    imports: [CommonModule, RouterModule],
+    declarations: [
+      CartIconComponent
+    ],
+    exports: [
+      CartIconComponent
+    ]
 })
 export class OrdersModule {
   constructor(cartServicr: CartService) {
