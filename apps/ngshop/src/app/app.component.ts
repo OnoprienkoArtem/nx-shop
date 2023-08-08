f2e5820445a686ed265d61ce1e2a3fe578cc686b
artemonoprienko <o.artem.e@gmail.com>
2023-08-08 23:54:47 +0300

    created build user session action

--- apps/ngshop/src/app/app.component.ts
+++ apps/ngshop/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UsersService } from '@bluebits/users';
 
 @Component({
-  selector: 'ngshop-root',
+  selector: 'bluebits-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
