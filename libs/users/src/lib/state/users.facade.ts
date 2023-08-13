eb4649b9078f00f8ec040efa0f8753fd148e844a
artemonoprienko <o.artem.e@gmail.com>
2023-08-13 23:59:27 +0300

    select user data

--- libs/users/src/lib/state/users.facade.ts
+++ libs/users/src/lib/state/users.facade.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Store } from '@ngrx/store';
-
+import { select, Store } from '@ngrx/store';
 import * as UsersActions from './users.actions';
+import * as UsersSelectors from './users.selectors';
 
 @Injectable()
 export class UsersFacade {
 
+    currentUser$ = this.store.pipe(select(UsersSelectors.getUser as any));
+    isAthenticated$ = this.store.pipe(select(UsersSelectors.getUserIsAuth as any));
 
     constructor(private readonly store: Store) {}
 
